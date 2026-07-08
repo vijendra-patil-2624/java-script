11917d2147c13fca521c7bea309f40586650516e
Vijendraa <vijendrapatil2006@gmail.com>
2026-07-08 18:38:46 +0530

    object

--- array_obj.js
+++ array_obj.js
@@ -1,13 +1,27 @@
-/* let frutes = ["apple", "bananna","mango", "grapes", "papaya"]
+ let frutes = ["apple", "bananna","mango", "grapes", "papaya"]
 
 console.log("1st frute is :",frutes[0],"last frute is:  ",frutes[4])
 
 frutes[0] = "ananas"
 frutes[4] = "strobarry"
 
-console.log(frutes)  */
+console.log(frutes)  
+let deleted = frutes.pop()
+console.log(deleted)
 
-let student = {
+console.log(frutes.pop())
+
+
+
+console.log(frutes.shift())
+frutes.unshift("vijay","sudeep")
+console.log(frutes)
+frutes.push("pramod")
+console.log(frutes)
+
+
+
+/* let student = {
     name : "vijendra",
     age : 20,
     course : "CS",
@@ -23,4 +37,4 @@ console.log(student["city"])
 
 student.ispass = "yes"
 
-console.log(student)
+console.log(student) */
---
+++ object.js
@@ -0,0 +1,17 @@
+let mobile = {
+    brand : "iQOO",
+    mobel : "Z9x",
+    prise : 13500,
+    color : "gray",
+    storage : "8 + 8 ",
+    isavailable : "yes",
+}
+
+console.log(mobile.brand,mobile.mobel)
+console.log(mobile["prise"])
+console.log(mobile.storage)
+
+console.log(mobile)
+
+mobile.prise = 15000;
+console.log(mobile)
